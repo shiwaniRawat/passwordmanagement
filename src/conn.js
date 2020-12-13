9dc59ad2165625ca137abbd09bd87a400cfc42b5
Shivani Butola <butolasony@gmail.com>
2020-12-13 12:28:03 +0530

    Add files via upload

---
+++ src/conn.js
@@ -0,0 +1,12 @@
+//connect db with express
+const mongoose=require("mongoose")
+mongoose.connect("mongodb://localhost:27017/comregistration",{
+    // to avoid DeprecationWarning
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true
+}).then(()=>{
+    console.log(`connection sucess`);
+}).catch((e)=>{
+    console.log(`server not sucess`)
+})
