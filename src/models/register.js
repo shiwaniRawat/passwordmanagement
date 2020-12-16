f877307e422fd9fb91aacbd6ddfef0f286568e17
Shivani Butola <butolasony@gmail.com>
2020-12-16 20:19:01 +0530

    Add files via upload

--- src/models/register.js
+++ src/models/register.js
@@ -1,10 +1,12 @@
 const mongoose=require("mongoose");
+const bcrypt=require("bcryptjs");
 
+//scama 
 const compregister= new mongoose.Schema({
-    userId :{
-        type:String,
-        required:true
-    },
+    // userId :{
+    //     type:String,
+    //     required:true
+    // },
     email:{
         type:String,
         required:true,
@@ -13,10 +15,28 @@ const compregister= new mongoose.Schema({
     password:{
         type:String,
         required:true
+    },
+    confirmpassword:{
+        type:String,
+        required:true
     }
 
 })
 
+compregister.pre("save", async function(next){
+    if (this.isModified("password")){
+            
+    // const passwordHash= await bcrypt.hash(password,10);
+    // console.log(`the current pass is ${this.password}`);
+    this.password =  await bcrypt.hash(this.password,10)
+    this.confirmpassword=undefined;
+    // console.log(`the current pass is ${this.password}`);
+
+    }
+
+    next();
+})
+
 //create collection
-const Register=new mongoose.model("Register",compregister);
-module.exports=Register;
+const Comp=new mongoose.model("Comp",compregister);
+module.exports=Comp;
